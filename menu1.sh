63cd983023eeb852813518ff9128fe2a6b258b2b
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2018-04-29 00:27:03 -0500

    Add files via upload

---
+++ menu1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+#Banner JC NOVATO
+clear 
+echo -e "+-----------------------------------------------+" 
+echo -e "+       SCRIPT INSTALADO SSH & Webmin           +" 
+echo -e "+                                               +"
+echo -e "+   Ubuntu 14,16 ,DEBIAN 7,8,9, 32bit & 64bit   +"
+echo -e "+                     VPS                       +"
+echo -e "+-----------------------------------------------+"
+echo -e "+             CONECTANDO EL MUNDO               +"
+echo -e "+-------------—---------------------------------+"
+echo -e "+                  VPS FULL                     +"
+echo -e "+              ADMIN JC NOVATO                  +"
+echo -e "+              INFORMACION VPS                  +"
+echo -e "+-----------------------------------------------+"
+echo -e " INFORMACION VPS" 
+echo -e "️ OpenSS port:22" 
+echo -e "️ Dropbea, port: 443"
+echo -e " Proxy Squid3 Port: 8080, 3128" 
+echo -e " Para juegos port: 7300"
+echo -e "️ Webmin: http://IPDeTuVps 10000/"
+echo -e "PARA VOLVER AL MENÚ DIGITE menu" 
+echo 
